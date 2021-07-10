e0c40d713f33c7679c9135622063c5a7f5418e3f
Carol <carolinedacosta.c@gmail.com>
2021-07-10 16:58:07 -0300

    add exercícios

---
+++ ex-1.go
@@ -0,0 +1,29 @@
+// Utilizando a palavra reservada var declare uma variável numérica do tipo int.
+// Atribua um valor a essa variável.
+// Printe na tela o seu valor.
+// Repita para 3 variáveis diferentes..
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num1 int
+	num1 = 7
+	fmt.Println(num1)
+
+	var num2 int
+	num2 = 9
+	fmt.Println(num2)
+
+	var num3 int
+	num3 = 55
+	fmt.Println(num3)
+
+	var num4 int
+	num4 = 22
+	fmt.Println(num4)
+
+}
---
+++ ex-2.go
@@ -0,0 +1,18 @@
+// Utilizando a marmota :=, declare duas variáveis: a e b.
+// Atribua os seguintes valores a elas, respectivamente: 230 e 27.
+// Crie variáveis para representar os resultados das operações matemáticas soma (a + b) e subtração (a - b).
+// Printe na tela os valores de todas as variáveis, um em cada linha..
+
+package main
+
+import "fmt"
+
+func main() {
+
+	num1 := 230
+	num2 := 27
+
+	fmt.Println(num1 + num2)
+	fmt.Println(num1 - num2)
+
+}

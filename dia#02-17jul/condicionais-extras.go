6a80a7907e8228f325cb6faa7836127c69639a26
Carol <carolinedacosta.c@gmail.com>
2021-07-25 19:40:31 -0300

    corrige exercício 2

--- dia#02-17jul/condicionais-extras.go
+++ dia#02-17jul/condicionais-extras.go
@@ -24,16 +24,19 @@ func main() {
 	// Exercício #02
 	// Faça um programa que testa se um número passado em uma variável é 0, múltiplo de 2, múltiplo de 3 ou nenhuma das opções.
 
-	numero := 20
+	numero := 25
 
-	if numero%2 != 0 {
+	if numero%2 == 0 {
 		fmt.Printf("O valor é múltiplo de 2")
 
-	} else if numero%3 != 0 {
+	} else if numero%3 == 0 {
 		fmt.Printf("O valor é múltiplo de 3")
 
-	} else {
+	} else if numero == 0 {
 		fmt.Printf("O valor é 0")
+
+	} else {
+		fmt.Printf("O valor não é 0 e nem multiplo de 2 ou 3")
 	}
 
 }
